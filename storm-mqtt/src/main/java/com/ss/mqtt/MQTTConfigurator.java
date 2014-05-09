353bd8c57bf5c5ea0ad539e4b4971ec8be298e5e
supunkamburugamuva <supun06@gmail.com>
2014-05-09 11:35:33 -0400

    small fix for logger

--- storm-mqtt/src/main/java/com/ss/mqtt/MQTTConfigurator.java
+++ storm-mqtt/src/main/java/com/ss/mqtt/MQTTConfigurator.java
@@ -3,9 +3,10 @@ package com.ss.mqtt;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import org.fusesource.mqtt.client.QoS;
 
+import java.io.Serializable;
 import java.util.List;
 
-public interface MQTTConfigurator {
+public interface MQTTConfigurator extends Serializable {
     String getURL();
 
     List<String> getQueueName();
--- storm-mqtt/src/main/java/com/ss/mqtt/MessageBuilder.java
+++ storm-mqtt/src/main/java/com/ss/mqtt/MessageBuilder.java
@@ -1,7 +1,8 @@
 package com.ss.mqtt;
 
+import java.io.Serializable;
 import java.util.List;
 
-public interface MessageBuilder {
+public interface MessageBuilder extends Serializable {
     List<Object> deSerialize(Message message);
 }
