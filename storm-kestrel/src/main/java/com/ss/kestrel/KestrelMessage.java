de8d8076f36f3036b1856c11204f74f71d9a5a59
supunkamburugamuva <supun06@gmail.com>
2014-05-12 15:29:05 -0400

    adding kestrel

--- storm-kestrel/src/main/java/com/ss/kestrel/KestrelMessage.java
+++ storm-kestrel/src/main/java/com/ss/kestrel/KestrelMessage.java
@@ -1,11 +1,4 @@
 package com.ss.kestrel;
 
-/**
- * Created with IntelliJ IDEA.
- * User: supun
- * Date: 5/12/14
- * Time: 3:24 PM
- * To change this template use File | Settings | File Templates.
- */
 public class KestrelMessage {
 }
