5cd27ecc37098c683b8a662bbb3e34e4549ccd94
Supun <supun06@gmail.com>
2014-05-23 01:51:49 -0400

    bolt for jms

---
+++ storm-rabbitmq/src/main/java/com/ss/rabbitmq/bolt/RabbitMQBolt.java
@@ -0,0 +1,26 @@
+package com.ss.rabbitmq.bolt;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+
+import java.util.Map;
+
+public class RabbitMQBolt extends BaseRichBolt {
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+
+    }
+}
